7172919b6f56311ba32fb74098c8ef7faecfc926
kanweitech <kanweiedward@gmail.com>
2023-05-31 08:46:09 -0700

    second commit

---
+++ contact/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from .forms import ContactForm
+
+def index(request):
+    form = ContactForm()
+
+    return render(request, 'contact/index.html', 'form': form)
